df9c8dca824bdf240e5e4f5244f5bebec0639031
Wemerson7 <wemerson_damasceno@yahoo.com>
2020-12-19 16:20:45 -0300

    Implementando ordenacao

--- app/src/main/java/com/example/netcine/service/FilmeService.java
+++ app/src/main/java/com/example/netcine/service/FilmeService.java
@@ -13,6 +13,9 @@ public interface FilmeService {
     @GET("tv/popular")
     Call<FilmesResult> obterSeriesPopulares(@Query("api_key") String chave_api);
 
+    @GET("movie/now_playing")
+    Call<FilmesResult> obterFilmesRecentes(@Query("api_key") String chave_api);
+
 
     @GET("trending/{media_type}/{time_window}")
     Call<FilmesResult> getTrending(@Path("media_type") String media_type, @Path("time_window") String time_window,
--- app/src/main/java/com/example/netcine/views/ui/FilmesFragment.java
+++ app/src/main/java/com/example/netcine/views/ui/FilmesFragment.java
@@ -2,33 +2,126 @@ package com.example.netcine.views.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.netcine.R;
 import com.example.netcine.adapters.AdapterFilme;
+import com.example.netcine.models.Filme;
+import com.example.netcine.service.ApiService;
+import com.example.netcine.service.FilmesResponse;
+import com.example.netcine.service.FilmesResult;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class FilmesFragment extends Fragment {
     RecyclerView rvFilmes;
+    PopupMenu popupMenu;
     AdapterFilme adapterFilme;
+    Button btnOrdenar;
+    String api_key = "6ee08abf792ae460668806133c782b4c";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_filmes, container, false);
 
+        btnOrdenar = root.findViewById(R.id.btnOrdenar);
         rvFilmes = root.findViewById(R.id.rvFilmes);
         adapterFilme = new AdapterFilme(getContext());
 
+        GridLayoutManager layoutManager1 = new GridLayoutManager(getContext(),2);
+        layoutManager1.setReverseLayout(false);
+        layoutManager1.setOrientation(RecyclerView.VERTICAL);
 
 
+        rvFilmes.setLayoutManager(layoutManager1);
+        rvFilmes.setAdapter(adapterFilme);
 
+
+        buscarFilmes();
+
+        btnOrdenar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupMenu = new PopupMenu(getContext(),btnOrdenar);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_sort, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.semana) {
+                            //OrdenarMes
+                            ordenarFilmes("week");
+                        } else if (item.getItemId() == R.id.dia) {
+                            //OrdenarDia
+                            ordenarFilmes("day");
+                            adapterFilme.notifyDataSetChanged();
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
 
         return root;
+    }
+
+    private void ordenarFilmes(String time_window) {
+        ApiService.getInstanceRetrofit().getTrending("movie",time_window,api_key)
+                .enqueue(new Callback<FilmesResult>() {
+                    @Override
+                    public void onResponse(Call<FilmesResult> call, Response<FilmesResult> response) {
+                        if (response.isSuccessful()) {
+
+                            for (FilmesResponse f : response.body().getResultadoFilmes()) {
+                                Filme filme = new Filme();
+                                filme.setCaminhoPosterFilme(f.getCaminhoPoster());
+                                filme.setTituloFilme(f.getTituloOriginal());
+
+                                adapterFilme.add(filme);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<FilmesResult> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    private void buscarFilmes() {
+        ApiService.getInstanceRetrofit().obterFilmesRecentes(api_key)
+                .enqueue(new Callback<FilmesResult>() {
+                    @Override
+                    public void onResponse(Call<FilmesResult> call, Response<FilmesResult> response) {
+                        if (response.isSuccessful()) {
+
+                            for (FilmesResponse f : response.body().getResultadoFilmes()) {
+                                Filme filme = new Filme();
+                                filme.setCaminhoPosterFilme(f.getCaminhoPoster());
+                                filme.setTituloFilme(f.getTituloOriginal());
+
+                                adapterFilme.add(filme);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<FilmesResult> call, Throwable t) {
+
+                    }
+                });
     }
 }
