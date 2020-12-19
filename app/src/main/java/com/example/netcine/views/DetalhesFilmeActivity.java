1bcad80785420d0fae5971d9410dadf3ee9303ff
Wemerson7 <wemerson_damasceno@yahoo.com>
2020-12-19 17:32:45 -0300

    Implementado ordenacao

--- app/src/main/java/com/example/netcine/views/DetalhesFilmeActivity.java
+++ app/src/main/java/com/example/netcine/views/DetalhesFilmeActivity.java
@@ -3,18 +3,28 @@ package com.example.netcine.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.example.netcine.R;
+import com.example.netcine.models.Filme;
 
 public class DetalhesFilmeActivity extends AppCompatActivity {
-
+Filme detalheMidia;
+TextView nomedoFilme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_filme);
 
+        nomedoFilme = findViewById(R.id.nomedofilme);
+        try {
+            detalheMidia = getIntent().getParcelableExtra("filme");
+            nomedoFilme.setText(detalheMidia.getTituloFilme());
 
-
+        }catch (Exception e){
+            Log.i("teste", e.getMessage());
+        }
 
 
 
